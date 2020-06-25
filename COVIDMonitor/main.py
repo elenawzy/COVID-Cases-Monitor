12288c7ab7946b4fe1519e50793ba52b31539c6a
Elena <elenawzy@gmail.com>
2020-06-25 14:40:42 -0400

    Added a basic landing page that can upload a file

--- COVIDMonitor/main.py
+++ COVIDMonitor/main.py
@@ -1,10 +1,10 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask("Assignment 2")
 
 @app.route('/monitor')
 def welcome_monitor():
-	return 'Welcome to the Covid Monitor!'
+	return render_template('monitor.html')
 
 if __name__ == "__main__":
 	app.run()
