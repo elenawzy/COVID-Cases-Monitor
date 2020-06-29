8deb3dda02acfaf3dfd03bf49887d4463667a2ff
liumin53 <gavin.liu@mail.utoronto.ca>
2020-06-29 16:53:08 -0400

    fixed import error in main.py

--- tests/unit_tests.py
+++ tests/unit_tests.py
@@ -1,7 +1,8 @@
 from COVIDMonitor.main import app
 
-def test_monitor():
-	response = app.test_client().get('/monitor')
 
-	assert response.status_code == 200
-	assert response.data == b'Welcome to the Covid Monitor!'
+def test_monitor():
+    response = app.test_client().get('/')
+
+    assert response.status_code == 200
+    assert response.data == b'Welcome to the Covid Monitor!'
