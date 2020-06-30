d041ebc18b9be8c2bb57e11b051b39b3ff0eed11
Elena <elenawzy@gmail.com>
2020-06-29 23:04:27 -0400

    Restructured file system for unit tests, also fixed typos in frontend

--- tests/unit_tests.py
+++ tests/unit_tests.py
@@ -1,8 +1,25 @@
-from COVIDMonitor.main import app
+from flask import Flask
 
+from COVIDMonitor.main import app
+from COVIDMonitor.project.routes import configure_routes
 
 def test_monitor():
-    response = app.test_client().get('/')
+	app = Flask(__name__)
+	configure_routes(app)
+	response = app.test_client().get('/')
 
-    assert response.status_code == 200
-    assert response.data == b'Welcome to the Covid Monitor!'
+	assert response.status_code == 200
+
+def test_filter_daily():
+	app = Flask(__name__)
+	configure_routes(app)
+	response = app.test_client().get('/filter-daily')
+
+	assert response.status_code == 200
+
+def test_filter_time_series():
+	app = Flask(__name__)
+	configure_routes(app)
+	response = app.test_client().get('/filter-time-series')
+
+	assert response.status_code == 200
